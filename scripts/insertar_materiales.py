1191b468552dd07a0509ef568b25ddc38ada4098
gilbertorodriguez59 <59020652+gilbertorodriguez59@users.noreply.github.com>
2026-08-15 17:27:42 -0600

    Extiende inserción física de materiales al capítulo 10

--- scripts/insertar_materiales.py
+++ scripts/insertar_materiales.py
@@ -10,6 +10,7 @@ CAPITULOS = {
     "07-variable-aleatoria-cdf.qmd": ("07-variable-aleatoria-cdf-colab.ipynb", "07-variable-aleatoria-cdf.Rmd", "cap07-variable-aleatoria-cdf.html"),
     "08-variables-discretas-pmf.qmd": ("08-variables-discretas-pmf-colab.ipynb", "08-variables-discretas-pmf.Rmd", "cap08-pmf-cdf.html"),
     "09-variables-continuas-densidad.qmd": ("09-variables-continuas-densidad-colab.ipynb", "09-variables-continuas-densidad.Rmd", "cap09-densidad-area.html"),
+    "10-esperanza-varianza-chebyshev.qmd": ("10-esperanza-varianza-chebyshev-colab.ipynb", "10-esperanza-varianza-chebyshev.Rmd", "cap10-esperanza-varianza.html"),
 }
 
 BASE = "https://github.com/gilbertorodriguez59/introduccion-probabilidad-r-geogebra-dev"
